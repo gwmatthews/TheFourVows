8c69d588f659ae2f2a9b1412375e544ed88c2365
GWMatthews <george.w.matthews@protonmail.com>
2020-12-01 11:31:22 -0500

    MESSAGE

--- pushit.sh
+++ pushit.sh
@@ -2,10 +2,23 @@
 
 MESSAGE=$1
 
+for file in kanzeon.tex leech-cards.tex sutra-styles.tex the-four-vows.tex the-four-vows-plain.tex the-heart-sutra.tex; do xelatex $file; done
+
+
+cd examples
+
+for i in ./*.tex; do xelatex $i; done
+
+cd ..
+
+./clean.sh
+
+./update.sh
+
 git add .
 git commit -m MESSAGE
 git push
-cp -rv examples/*.pdf ../../github/gwmatthews.github.io.git/
+cp -rv examples/*.pdf ../../github/gwmatthews.github.io.git/examples
 cp -v *.pdf ../../github/gwmatthews.github.io.git/
 
 cd ../../github/gwmatthews.github.io.git/
