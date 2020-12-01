700dd0303e4ed133727b14dd592fd25c3cf307e9
GWMatthews <george.w.matthews@protonmail.com>
2020-12-01 10:58:25 -0500

    MESSAGE

---
+++ pushit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+MESSAGE=$1
+
+git add .
+git commit -m MESSAGE
+git push
+cp -rv examples/*.pdf ../../github/gwmatthews.github.io.git/
+gwmatthews.github.io.git$ cp -v *.pdf ../../github/gwmatthews.github.io.git/
+
+cd ../../github/gwmatthews.github.io.git/
+git add .
+git commit -m MESSAGE
+git push
