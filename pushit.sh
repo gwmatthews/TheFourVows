eaa12465fd50cb8242b8812cb38012123bc1862b
GWMatthews <george.w.matthews@protonmail.com>
2020-12-01 11:01:49 -0500

    MESSAGE

--- pushit.sh
+++ pushit.sh
@@ -6,7 +6,7 @@ git add .
 git commit -m MESSAGE
 git push
 cp -rv examples/*.pdf ../../github/gwmatthews.github.io.git/
-gwmatthews.github.io.git$ cp -v *.pdf ../../github/gwmatthews.github.io.git/
+cp -v *.pdf ../../github/gwmatthews.github.io.git/
 
 cd ../../github/gwmatthews.github.io.git/
 git add .
