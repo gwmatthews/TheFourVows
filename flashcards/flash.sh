f883d05c46701def32019e19a5ed02c9a9327b91
GWMatthews <george.w.matthews@protonmail.com>
2020-12-04 13:24:18 -0500

    rebuild

--- flashcards/flash.sh
+++ flashcards/flash.sh
@@ -1,32 +1,75 @@
 #!/bin/bash
-SET=$1                                            ## takes one argument filename of csv file minus the .csv
-BEGINFRONT=$(sed -n '1,4 p' < card-frame)         ## LaTeX at beginning of front pages
-BEGINBACK=$(sed -n '5,8 p' < card-frame)          ## LaTeX at beginning of back pages
-END=$(sed -n '9,11 p' < card-frame)               ## LaTeX at end of file
-
+###  generates Japanese flashcards from csv files produced by Wanikani Item Inspector
+###  EXAMPLE flash.sh vocab-20   (using file vocab-20.csv)
+SET=$1                                                                ## takes one argument filename of csv file minus the .csv
+BEGINFRONT=$(sed -n '1,4 p' < card-frame)                             ## LaTeX at beginning of front pages
+BEGINBACK=$(sed -n '5,8 p' < card-frame)                              ## LaTeX at beginning of back pages
+END=$(sed -n '9,11 p' < card-frame)                                   ## LaTeX at end of cards file
+#
 ## fill content into front of card: wrap comma separated fields in braces, add prefix, divide into sections, same for back
-
+#
 FRONT=$(cat $SET.csv | sed -e 's_Unavailable_{radical_'| sed -e 's/["]/{/' -e 's/[",]./}{/' -e 's/[,"]./}{/' -e 's/"/}/' | sed 's/{/\\flashFront{/' | sed -f spacing.sed| sed '/3/ a\\\RLmulticolcolumns')
 BACK=$(cat $SET.csv | sed -e 's_Unavailable_{radical_'| sed -e 's/["]/{/' -e 's/[",]./}{/' -e 's/[,"]./}{/' -e 's/"/}/' | sed 's/{/\\flashBack{/' | sed -f spacing.sed| sed '/3/ a\\\LRmulticolcolumns')
-
+#
 ## add front and back of card LaTeX wrappers
-
+#
 echo "$BEGINFRONT" "$FRONT" "$END" > $1-cards-front.tex
 echo "$BEGINBACK" "$BACK" "$END"  > $1-cards-back.tex
-
+#
 ## duplicate the cards template
-
+#
 cp cards.tex $1-cards.tex
-
-## compile cards
-
+#
+## create temp pdf sheets to be reshuffled and rebuilt in code that follows
+#
 xelatex $1-cards.tex
-
+#
+## reshuffle pages for two sided printing
+#
+## initialize variables
+#
+PAGES=$(pdfinfo $SET-cards.pdf | grep "Pages" | awk '{print $NF}')    ## Extract number of paages in cards file                                                                           
+REVERSE=$(( PAGES / 2 ))
+OUTPUT=pagelist.tex                                                   ## For building reshuffled page set
+NUM=0
+#
+## copy the template
+#
+cp remix.tex $SET.tex                                                 
+#
+## construct the pagelist array
+#
+arr=()                                                                
+for ((i=1; i<=$REVERSE; i++)); do
+    NUM=$i
+	arr+=("$NUM" )
+    NUM=$(( REVERSE + i))
+    arr+=("$NUM" )
+done
+#
+##### format for pdfpages
+#
+PAGELIST=$(echo ${arr[@]} | sed 's/\s/, /g')                          
+#
+## assemble full line of LaTeX code
+#
+REORDERED="\includepdf[pages={${PAGELIST}}, delta=0 0, offset=0 0]{$SET-cards.pdf}"   
+#
+## export as pagelist.tex
+#
+echo $REORDERED > $OUTPUT                                             
+#
+## compile reordered flashcard pages
+#
+xelatex $SET.tex                                                     
+#
 ## clean up the mess
-
+#
 rm ./*.aux
 rm ./*.log
 rm ./*.out
 rm $SET-cards*.tex
+rm $SET-cards.pdf
+
 
 
--- flashcards/remix.sh
+++ flashcards/remix.sh
@@ -1,25 +1,29 @@
-#!/bin/bash
-TITLE=$1   
-PAGES=$2  
+#!/bin/bash    
+SET=$1                                                                ### takes single argument -- filename without extenstion
+PAGES=$(pdfinfo $SET-cards.pdf | grep "Pages" | awk '{print $NF}')    ### Extract number of paages in card file                                                                           
+REVERSE=$(( PAGES / 2 ))
 
-A=1
-B="$PAGES / 2 + 1"
+OUTPUT=pagelist.tex                                                   ### what this script does is generate a snippet of LaTeX code to be inserted into the document template
+NUM=0
+cp remix.tex $SET.tex                                                 ### copy the template
 
-PAGELIST=($A,$B)
+arr=()                                                                ### construct the pagelist array
+for ((i=1; i<=$REVERSE; i++)); do
+    NUM=$i
+	arr+=("$NUM" )
+    NUM=$(( REVERSE + i))
+    arr+=("$NUM" )
+done
+PAGELIST=$(echo ${arr[@]} | sed 's/\s/, /g')                          ### format for pdfpages
 
-x=1
-while [[ $x -lt $PAGES ]]
-do
-  A=$A+1
-PAGELIST+=($A)
-B=$B+1
-PAGELIST+=($B)
-x=$(( $x + 2 ))
-done 
+REORDERED="\includepdf[pages={${PAGELIST}}, delta=0 0, offset=0 0]{$SET-cards.pdf}"  ### assemble full line of LaTeX code 
+
+echo $REORDERED > $OUTPUT                                             ### export as pagelist.tex
+
+xelatex $SET.tex                                                      ### compile reordered flashcard pages
 
 
+#### NOW INTEGRATED into flash.sh keeping in case it is needed elsewhere
 
 
-
-echo $PAGELIST
 
